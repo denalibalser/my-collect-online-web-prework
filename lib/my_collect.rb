78664375b46463d9e537c1acbeeade258c8a9c4a
Denali Balser <denali.balser167@gmail.com>
2019-04-01 16:13:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,9 @@
-def my_collect(languages)
-  
-
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+    i += 1
+  end
+  name_collection
 end
